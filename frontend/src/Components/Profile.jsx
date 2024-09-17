9b4dd4dfbee5f7dee6b2699c23725d3bce4b5822
junaid <junaidsheikh45@gmail.com>
2024-09-17 18:35:09 +0530

    follower & following functionality added

--- frontend/src/Components/Profile.jsx
+++ frontend/src/Components/Profile.jsx
@@ -1,45 +1,90 @@
-import React from 'react'
+import React from 'react';
 import Avatar from 'react-avatar';
-import { IoIosArrowRoundBack } from "react-icons/io";
-import { Link, useParams } from 'react-router-dom';
-import {useSelector} from 'react-redux'
-import useGetProfile from '../Hooks/useGetProfile';
+import { IoMdArrowBack } from "react-icons/io";
+import { Link, useParams} from "react-router-dom"
+import { useDispatch, useSelector } from 'react-redux';
+import useGetProfile from "../Hooks/useGetProfile"
+import axios from 'axios';
+import { USER_API_END_POINT } from '../utils/Constants';
+import toast from 'react-hot-toast'; 
+import { followingUpdate } from '../Redux/userSlice';
+import { getRefresh } from '../Redux/tweetSlice';
 
 function Profile() {
-
     const { user, profile } = useSelector(store => store.user)
     // console.log(user?._id);
     const {id} = useParams()
      useGetProfile(id)
+     const dispatch = useDispatch()
 
+     const followAndUnfollowHandler = async () => {
+
+        if (user.following.includes(id)) {
+            //unfollow
+            try {
+                const res = await axios.post(`${USER_API_END_POINT}/unfollow/${id}`, {id:user?._id}, {
+                    withCredentials:true
+                })
+                console.log(res);
+                dispatch(followingUpdate(id))
+                dispatch(getRefresh())
+                toast.success(res.data.message)
+            } catch (error) {
+                toast.error(error.response.data.message)
+                console.log(error);
+            }
+        } else {
+            //follow
+            try {
+                const res = await axios.post(`${USER_API_END_POINT}/follow/${id}`, {id:user?._id}, {
+                    withCredentials:true
+                })
+                console.log(res);
+                dispatch(followingUpdate(id))
+                dispatch(getRefresh())
+                toast.success(res.data.message)
+            } catch (error) {
+                toast.error(error.response.data.message)
+                console.log(error);
+            }
+            
+        }
+     }
 
     return (
-        <div className='w-[60%]'>
-            <div className='flex'>
-                <Link to={"/"}>
-                    <IoIosArrowRoundBack size={40} className='hover:bg-gray-300 cursor-pointer rounded-full my-2'/>
-                </Link>
-                <div>
-                    <h1 className='font-bold ml-4 mt-2'>{profile?.name}</h1>
-                    <p className='ml-4 text-lg'>10 Posts</p>
+        <div className='w-[60%] border-l border-r border-gray-200'>
+            <div>
+                <div className='flex items-center py-2'>
+                    <Link to="/" className="p-2 rounded-full hover:bg-gray-100 hover:cursor-pointer">
+                        <IoMdArrowBack size="24px" />
+                    </Link>
+                    <div className='ml-2'>
+                        <h1 className='font-bold text-lg'>{profile?.name}</h1>
+                        <p className='text-gray-500 text-sm'>10 post</p>
+                    </div>
+                </div>
+                <img src="https://pbs.twimg.com/profile_banners/1581707412922200067/1693248932/1080x360" alt="banner" />
+                <div className='absolute top-52 ml-2 border-4 border-white rounded-full'>
+                    <Avatar src="https://pbs.twimg.com/profile_images/1703261403237502976/W0SFbJVS_400x400.jpg" size="120" round={true} />
+                </div>
+                <div className='text-right m-4'>
+                    {
+                        profile?._id === user?._id ? (
+                            <button className='px-4 py-1 hover:bg-gray-200 rounded-full border border-gray-400'>Edit Profile</button>
+
+                        ) : (
+                            <button onClick={followAndUnfollowHandler} className='px-4 py-1 bg-black text-white rounded-full'>{user.following.includes(id) ? "Following" : "Follow"}</button>
+                        )
+                    }
+                </div>
+                <div className='m-4'>
+                    <h1 className='font-bold text-xl'>{profile?.name}</h1>
+                    <p>{`@${profile?.username}`}</p>
+                </div>
+                <div className='m-4 text-sm'>
+                    <p>🌐 Exploring the web's endless possibilities with MERN Stack 🚀 | Problem solver by day, coder by night 🌙 | Coffee lover ☕ | Join me on this coding journey!</p>
                 </div>
             </div>
-                <div>
-                    <img src="banner1.jfif" alt="banner" className='w-full rounded-md cursor-pointer'/>
-                    <div className='absolute top-60 rounded-full border-2 border-white p-0 ml-4'>
-                        <Avatar className='' src='lion2.avif' size='120' round={true}/>
-                    </div>
-                    <div className='text-right m-4'>
-                        <button className='px-4 py-1 text-white bg-black rounded-full'>Edit Profile</button>
-                    </div>
-                    <div className='ml-0 mt-2'>
-                        <h1 className='font-bold text-xl'>{profile?.name}</h1>
-                        <p>{`@${profile?.username}`}</p>
-                    </div>
-                    <div className='ml-0 mt-3'  >
-                        <p className='text-lg'>this is a bio of this application</p>
-                    </div>
-                </div>
         </div>
     )
 }
